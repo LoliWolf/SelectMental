4ce4cc5f6f7bf9907add850594f733b3515566d9
LoliWolf <2233284845@qq.com>
2022-11-21 13:52:41 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SelectMental
 QQBot查询精神状态接口
+
+## 调用方法
+post请求
+端口号:23025
+body:
+{
+    "QQ":qq号
+}
---
+++ src/main/java/com/demo/SelectMentalApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectMentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectMentalApplication.class, args);
+    }
+
+}
